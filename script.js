725d4b976257e4bd44cb443fe7ea136d57da93f0
WILbeerT-B <wilbertbundalian@gmail.com>
2023-04-07 22:53:26 -0700

    Modify all files
    HTML - Add (1)main container to center all contents;
    (2)buttons to choose the grid size; (3)footer section.
    
    CSS - Add styles to buttons, grids and footer.
    
    JS - Create functions (1)to choose the grid size,
    (2)to generate the grid.

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-const container = document.querySelector('.container');
-
-for (let i = 0; i < 256; i++) {
-    const divSquare = document.createElement('div');
-    divSquare.classList.add('color');
-    container.appendChild(divSquare);   
-}
-
-const colorDivs = document.getElementsByClassName('color');
-for(let i=0; i<colorDivs.length; i++) {
-    colorDivs[i].addEventListener('mouseleave', () => {
-        colorDivs[i].style.backgroundColor = "black";
+function chooseGrid() {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (button.classList.contains('grid-8')) {
+                generateGrid(8 * 8, 'grid-8x8');
+            } else if (button.classList.contains('grid-16')) {
+                generateGrid(16 * 16, 'grid-16x16');
+            } else if (button.classList.contains('grid-24')) {
+                generateGrid(24 * 24, 'grid-24x24');
+            }
+        });
     });
 }
+
+function generateGrid(divNum = 16 * 16, className = 'grid-16x16') {
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = "";
+    for (let i = 0; i < divNum; i++) {
+        const divSquare = document.createElement('div');
+        gridContainer.classList.remove('grid-8x8', 'grid-16x16', 'grid-24x24');
+        divSquare.classList.add(className);
+        gridContainer.appendChild(divSquare);
+        //paint the grid with black
+        let colorDivs = document.getElementsByClassName(className);
+        for(let i=0; i<colorDivs.length; i++) {
+            colorDivs[i].addEventListener('mouseenter', () => {
+                colorDivs[i].style.backgroundColor = "black";
+            });
+        }
+    }
+}
+
+chooseGrid();
+generateGrid();
